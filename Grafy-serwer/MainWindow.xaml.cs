4467c8f9317fe45662b751131ce26ac625346e0e
Makrol <36311236+Makrol@users.noreply.github.com>
2024-03-28 10:56:36 +0100

    Test uzyskiwania połączenie pomiędzy klientem, a serwerem przez TCP

---
+++ Grafy-serwer/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Grafy_serwer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        static TcpListener listener;
+        const int port = 8888;
+        static int clientCount = 0;
+        static List<Thread> clientThreads = new List<Thread>();
+        private Thread serverThred;
+        public MainWindow()
+        {
+            InitializeComponent();
+            counterTextBlock.Text = "Ilość połączonych klientów: 0";
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            serverThred = new Thread(new ParameterizedThreadStart(StartServer));
+            serverThred.Start();
+            startButton.IsEnabled = false;
+        }
+        void StartServer(object obj)
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+                Console.WriteLine("Serwer uruchomiony. Oczekiwanie na połączenia...");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    Thread newClientThread = new Thread(new ParameterizedThreadStart(HandleClient));
+                    clientThreads.Add(newClientThread);
+                    newClientThread.Start(client);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (listener != null)
+                    listener.Stop();
+            }
+        }
+        void UpdateCounter()
+        {
+            clientCount++;
+            Dispatcher.Invoke(() => { counterTextBlock.Text = "Ilość połączonych klientów: " + clientCount; });
+        
+        }
+        void HandleClient(object obj)
+        {
+            
+            TcpClient client = (TcpClient)obj;
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            try
+            {
+                UpdateCounter();
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("Odebrano: " + dataReceived);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                stream.Close();
+                client.Close();
+            }
+        }
+    }
+}
---
+++ Grafy/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Grafy
+{
+    public partial class MainWindow : Window
+    {
+        private bool isDragging = false;
+        private Ellipse selectedPoint = null;
+        private List<Ellipse> points = new List<Ellipse>();
+        private int mode = 1;
+        private List<Line> lines = new List<Line>();
+        private List<Point> linePoint = new List<Point>();
+
+
+        public MainWindow()
+        {
+            //InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Ustawienie adresu IP i portu serwera
+                string serverIP = "127.0.0.1"; // Adres IP serwera
+                int port = 8888; // Port serwera
+
+                // Utworzenie klienta TCP
+                TcpClient client = new TcpClient(serverIP, port);
+                Console.WriteLine("Nawiązano połączenie z serwerem.");
+
+                // Utworzenie strumienia sieciowego do komunikacji z serwerem
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    // Wprowadzenie wiadomości od użytkownika
+                    Console.Write("Wprowadź wiadomość: ");
+                    string message = Console.ReadLine();
+
+                    // Konwersja wiadomości na tablicę bajtów
+                    byte[] dataToSend = Encoding.ASCII.GetBytes(message);
+
+                    // Wysłanie wiadomości do serwera
+                    stream.Write(dataToSend, 0, dataToSend.Length);
+
+                    // Odebranie odpowiedzi od serwera
+                    byte[] responseData = new byte[1024];
+                    int bytesRead = stream.Read(responseData, 0, responseData.Length);
+                    string responseMessage = Encoding.ASCII.GetString(responseData, 0, bytesRead);
+                    Console.WriteLine("Odpowiedź od serwera: " + responseMessage);
+                }
+
+                // Zamknięcie połączenia
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Błąd: " + ex.Message);
+            }
+        }
+    }
+}
